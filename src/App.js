f7c73b086118fbd4f28777f1a684920007837f0c
drajendi <drajendi@cisco.com>
2023-07-18 22:17:40 +0530

    Updated the router config

--- src/App.js
+++ src/App.js
@@ -8,73 +8,49 @@ import Home from './components/home/Home';
 import MyProfile from './components/profile/MyProfile';
 import OrderHistory from './components/orderhistory/OrderHistory';
 import Productinfo from './components/product_details/Productinfo'
-import { BrowserRouter as Router,
-Switch,
-Route} from "react-router-dom"
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { useState } from 'react';
 import CartItem from './components/cart/CartItem';
 import Shop from './components/shop/Shop';
 import AllCategory from './components/ItemsByCategory/AllCategory';
 import Categories from './components/ItemsByCategory/seperateCategory/Categories';
 import SearchPage from './components/search/SearchPage';
+import PrivateRoutes from './routes/PrivateRoutes';
 function App() {
-  const [states,setState]=useState(true);
+  const [states, setState] = useState(true);
   return (
     <div className="App">
-    <Router>
+      <BrowserRouter>
       <div className='top'>
         <Header states={states} change={setState} />
         <Navbar/>
       </div>
-     <Switch>
-        <Route exact path="/">
-           <Home/>
-        </Route>
-          <Route path="/signin">
-           <SignIn changestate={data=>setState(data)}/>
-          </Route>
-          <Route path="/signup">
-           <SignUp/>
-          </Route>
-          <Route path="/myprofile">
-            <MyProfile/>
-          </Route>
-          <Route path="/myorders">
-            <OrderHistory/>
-          </Route>
-           <Route path="/productDetails/:value">
-          <Productinfo states={states}/>
-        </Route>
-       <Route path="/cart">
-          <CartItem />
-        </Route>
-        </Switch>
-       <Route path="/shop">
-          <Shop/>
-        </Route>
-         <Route path="/search">
-          <SearchPage/>
-        </Route>
-        <Route path="/categories">
-          <AllCategory/>
-        </Route>
-          <Route path="/laptops">
-          <Categories categoryName="laptop"/>
-        </Route>
-        <Route path="/phones">
-          <Categories categoryName="phone"/>
-        </Route>
-        <Route path="/camera">
-          <Categories categoryName="camera"/>
-        </Route>
-        <Route path="/accessories">
-          <Categories categoryName="accessories"/>
-        </Route>
-        <Route path="/watch">
-          <Categories categoryName="watch"/>
-        </Route> 
-        <Footer/>  
-      </Router>
+        <Routes>
+          {/* <Route path='/'> */}
+            <Route index element={<Home />} />
+            <Route path='signin' element={<SignIn changestate={data => setState(data)} />} />
+            <Route path='signup' element={<SignUp />} />
+            <Route element = { <PrivateRoutes auth={!states} />}>
+            <Route path='cart' element={<CartItem />} />
+            <Route path='myprofile' element={<MyProfile />} />
+            <Route path='orderhistory' element={<OrderHistory />} />
+            <Route path="productDetails/:value" element={<Productinfo states={states} />} />
+            <Route path='shop' element={<Shop />} />
+            <Route path="search" element={<SearchPage />} />
+            <Route path="categories">
+              <Route index element={<AllCategory />} />
+              <Route path="laptops" element={<Categories categoryName="laptop" />} />
+              <Route path="phones" element={<Categories categoryName="phone" />} />
+              <Route path="camera" element={<Categories categoryName="camera" />} />
+              <Route path="accessories" element={<Categories categoryName="accessories" />} />
+              <Route path="watch" element={<Categories categoryName="watch" />} />
+            </Route>
+            </Route>
+          {/* </Route> */}
+        </Routes>
+        <Routes/>
+        <Footer/> 
+      </BrowserRouter>
     </div>
   );
 }
